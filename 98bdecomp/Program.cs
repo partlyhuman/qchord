77a9c2d25dda4545719821cd73bd3ea53610781b
Roger Braunstein <roger@partlyhuman.com>
2024-09-07 21:07:45 -0700

    Instrument tables

--- 98bdecomp/Program.cs
+++ 98bdecomp/Program.cs
@@ -1,24 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-
+﻿using System.Runtime.InteropServices;
 using _98bdecomp;
 
+byte[] bytes = File.ReadAllBytes(args[0]);
+// Stream stream = new MemoryStream(bytes);
+Span<byte> buffer = bytes.AsSpan();
 
-
-FileStream stream = File.OpenRead(args[0]);
-
-// while (stream.Position != stream.Length)
-// {
-    RiffHeader header = Utils.ReadStruct<RiffHeader>(stream);
-    Console.WriteLine($"Chunk {header.Id} length {header.Length}");
-    switch (header.Id)
+for (int offset = 0; offset < buffer.Length;)
+{
+    RiffChunk chunk = new RiffChunk(bytes, ref offset);
+    Console.WriteLine($"Chunk {chunk.Header.Id} length {chunk.Data.Length}");
+    switch (chunk.Header.Id)
     {
         case "PRIO":
-            var prio = Utils.ReadStruct<PrioChunk>(stream);
+        {
+            var prio = MemoryMarshal.Read<PriorityData>(chunk.Data);
             Console.WriteLine($"Soundbank {prio.Name} priority {prio.Priority}");
             break;
-        
+        }
+
         case "IMAP":
-            
+        {
+            int split1 = chunk.Data.IndexOf(stackalloc byte[] { 0xff, 0xff });
+            int split2 = chunk.Data.LastIndexOf(stackalloc byte[] { 0xff, 0xff });
+
+            Span<byte> instrumentTableSpan = chunk.Data[..split1];
+            Span<byte> drumsetTableSpan = chunk.Data.Slice(split1 + 2, split2 - split1);
+
+            Span<InstrumentTableData> instrumentTable = MemoryMarshal.Cast<byte, InstrumentTableData>(instrumentTableSpan);
+            Span<InstrumentTableData> drumsetTable = MemoryMarshal.Cast<byte, InstrumentTableData>(drumsetTableSpan);
+
+            Console.WriteLine($"Instrument table found at {offset + split1:x8} + {instrumentTableSpan.Length:x8}");
+            Console.WriteLine($"with {instrumentTable.Length} instruments");
+            Console.WriteLine($"Drumset table found at {offset + split2 + 2:x8} + {drumsetTableSpan.Length:x8}");
+            Console.WriteLine($"with {drumsetTable.Length} instruments");
             break;
+        }
+        default:
+            Console.WriteLine("Unimplemented, stopping");
+            return;
     }
-// }
+}
--- 98bdecomp/RiffChunk.cs
+++ 98bdecomp/RiffChunk.cs
@@ -1,6 +1,6 @@
 using System.Diagnostics;
 using System.Runtime.InteropServices;
-using System.Text;
+using static System.Text.Encoding;
 
 namespace _98bdecomp;
 
@@ -10,7 +10,12 @@ public static class Utils
     {
         Span<byte> buffer = stackalloc byte[sizeof(T)];
         ReadSpan(stream, ref buffer);
-        return MemoryMarshal.AsRef<T>(buffer);
+        return MemoryMarshal.Read<T>(buffer);
+    }
+    public static unsafe T ReadStruct<T>(Span<byte> buffer, int start) where T : unmanaged
+    {
+        Span<byte> span2 = buffer.Slice(start, sizeof(T));
+        return MemoryMarshal.Read<T>(span2);
     }
 
     public static void ReadSpan(Stream stream, ref Span<byte> buffer)
@@ -24,27 +29,48 @@ public unsafe struct RiffHeader
     private fixed byte id[4];
     public UInt32 Length;
     
+    public static int Size => sizeof(RiffHeader);
     public string Id
     {
         get
         {
-            fixed (byte* idPtr = id) return Encoding.ASCII.GetString(idPtr, 4);
+            fixed (byte* idPtr = id) return ASCII.GetString(idPtr, 4);
         }
     }
 }
 
+ref struct RiffChunk
+{
+    public RiffHeader Header;
+    public Span<byte> Data;
 
-public unsafe struct PrioChunk
+    public RiffChunk(Span<byte> span, ref int offset)
+    {
+        Header = MemoryMarshal.Read<RiffHeader>(span.Slice(offset, RiffHeader.Size));
+        offset += RiffHeader.Size;
+        Data = span.Slice(offset, offset + (int)Header.Length);
+        offset += (int)Header.Length;
+    }
+}
+
+public struct InstrumentTableData
+{
+    public UInt16 ProgramNumber;
+    public byte VariationNumber;
+    public byte ParamPageNumber;
+    public UInt16 ParamPagePointer;
+}
+
+public unsafe struct PriorityData
 {
     private fixed byte name[8];
     public UInt16 Priority;
+    
     public string Name
     {
         get
         {
-            fixed (byte* namePtr = name) return Encoding.ASCII.GetString(namePtr, 8);
+            fixed (byte* namePtr = name) return ASCII.GetString(namePtr, 8);
         }
     }
-
-    
 }
